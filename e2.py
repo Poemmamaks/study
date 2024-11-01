65112babcbdb06b99765dbae7bceae2c3cfaa5e5
Poemmamaks <super.permitin@gmail.com>
2024-11-01 16:52:15 +0300

    hgh

---
+++ e2.py
@@ -0,0 +1,16 @@
+#program1
+print(9 ** 0.5 * 5)
+
+#program2
+print(9.98 < 9.99 and 1000 != 1000.1)
+
+#program3
+print(2 * (2 + 2))
+
+#program4
+a = '123.456'
+b = float(a)
+c = b * 10
+d = int(c)
+e = d % 10
+print(e)
